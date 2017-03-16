50e7210cb49eba843bfff84ffa65440bf5abcbfe
whoami <yar.spirin@gmail.com>
2017-03-16 21:22:44 +0300

    Initial Commit

---
+++ DrawShapes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawShapes
+//
+//  Created by whoami on 3/16/17.
+//  Copyright © 2017 Mountain Viewer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
